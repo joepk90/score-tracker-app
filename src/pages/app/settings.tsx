3bd1b69eb87213b8103d9d3e17cd528801f0d2c5
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2023-04-15 21:10:19 +0100

    disable alternative sidebars on settings page. disable profile route configuration + content option

--- src/pages/app/settings.tsx
+++ src/pages/app/settings.tsx
@@ -1,34 +1,18 @@
-import AccountBoxIcon from '@mui/icons-material/AccountBox'
 import Box from '@mui/material/Box'
-import Divider from '@mui/material/Divider'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
-import ListItemIcon from '@mui/material/ListItemIcon'
 import ListItemText from '@mui/material/ListItemText'
 import Paper from '@mui/material/Paper'
 import Typography from '@mui/material/Typography'
-import { SettingsProfile, ShellTitle } from 'components'
 import { AppLayout } from 'components/layout'
-import { MobileMenuItem } from 'components/sidebars'
-import { useRouter } from 'next/router'
-import { ReactNode } from 'react'
 import { Actions, useAppShell } from 'components/providers/AppShellProvider'
 import Hidden from '@mui/material/Hidden'
 import Switch from '@mui/material/Switch'
 import { ThemeSwitch } from 'components/ThemeSwitch'
+import { ShellTitle } from 'components'
 
 export default function Settings() {
-  const router = useRouter()
-
-  return (
-    <>
-      {router.query.section === 'profile' ? (
-        <SettingsProfile />
-      ) : (
-        <MainContent />
-      )}
-    </>
-  )
+  return <MainContent />
 }
 
 const MainContent = () => {
@@ -89,46 +73,46 @@ const MainContent = () => {
 
 Settings.layout = AppLayout
 
-Settings.desktopSidebar = function SettingsMenuDesktop(
-  defaultItems: ReactNode
-) {
-  return (
-    <>
-      <List>{defaultItems}</List>
-      <Divider />
-      <List>
-        <MobileMenuItem
-          icon={
-            <ListItemIcon>
-              <AccountBoxIcon />
-            </ListItemIcon>
-          }
-          link={{ href: '/app/settings?section=profile', shallow: true }}
-        >
-          Profile
-        </MobileMenuItem>
-      </List>
-    </>
-  )
-}
+// Settings.desktopSidebar = function SettingsMenuDesktop(
+//   defaultItems: ReactNode
+// ) {
+//   return (
+//     <>
+//       <List>{defaultItems}</List>
+//       <Divider />
+//       <List>
+//         <MobileMenuItem
+//           icon={
+//             <ListItemIcon>
+//               <AccountBoxIcon />
+//             </ListItemIcon>
+//           }
+//           link={{ href: '/app/settings?section=profile', shallow: true }}
+//         >
+//           Profile
+//         </MobileMenuItem>
+//       </List>
+//     </>
+//   )
+// }
 
-Settings.mobileSidebar = function SettingsMenuMobile(defaultItems: ReactNode) {
-  return (
-    <>
-      <List>{defaultItems}</List>
-      <Divider />
-      <List>
-        <MobileMenuItem
-          icon={
-            <ListItemIcon>
-              <AccountBoxIcon />
-            </ListItemIcon>
-          }
-          link={{ href: '/app/settings?section=profile', shallow: true }}
-        >
-          Profile
-        </MobileMenuItem>
-      </List>
-    </>
-  )
-}
+// Settings.mobileSidebar = function SettingsMenuMobile(defaultItems: ReactNode) {
+//   return (
+//     <>
+//       <List>{defaultItems}</List>
+//       <Divider />
+//       <List>
+//         <MobileMenuItem
+//           icon={
+//             <ListItemIcon>
+//               <AccountBoxIcon />
+//             </ListItemIcon>
+//           }
+//           link={{ href: '/app/settings?section=profile', shallow: true }}
+//         >
+//           Profile
+//         </MobileMenuItem>
+//       </List>
+//     </>
+//   )
+// }
