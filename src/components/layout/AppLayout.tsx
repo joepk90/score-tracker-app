6a1acaa6f37e6a3a11664956191dbd633cbe572c
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2023-04-16 18:04:14 +0100

    add redux store Provider to AppLayout

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,16 +1,21 @@
 import { AppShell } from 'components/layout'
 import { AppShellProvider } from 'components/providers/AppShellProvider'
 import { NextApplicationPage } from 'pages/_app'
+import { store } from 'store/store'
+import { auth } from 'store/auth'
+import { Provider } from 'react-redux'
 
 export function AppLayout(Component: NextApplicationPage, props: any) {
   return (
-    <AppShellProvider>
-      <AppShell
-        mobileSidebar={Component.mobileSidebar}
-        desktopSidebar={Component.desktopSidebar}
-      >
-        <Component {...props} />
-      </AppShell>
-    </AppShellProvider>
+    <Provider store={store}>
+      <AppShellProvider>
+        <AppShell
+          mobileSidebar={Component.mobileSidebar}
+          desktopSidebar={Component.desktopSidebar}
+        >
+          <Component {...props} />
+        </AppShell>
+      </AppShellProvider>
+    </Provider>
   )
 }
